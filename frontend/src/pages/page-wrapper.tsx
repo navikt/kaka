5ca7836bbdd125b438a89464202a95f20c0e240d
Christian Skrøvseth <christian.skrovseth@nav.no>
2021-11-26 14:53:03 +0100

    Make sure there is space for the action bar

--- frontend/src/pages/page-wrapper.tsx
+++ frontend/src/pages/page-wrapper.tsx
@@ -3,6 +3,6 @@ import styled from 'styled-components';
 export const PageWrapper = styled.article`
   padding-top: 2em;
   padding-left: 3em;
-  padding-bottom: 3em;
+  padding-bottom: 80px;
   overflow-y: auto;
 `;
