62a509fffbc9b98987b576caee223150c5aa87db
Daniel Eriksson <daniel.eriksson@nav.no>
2021-12-15 10:28:14 +0100

    Reset validation state when a kvalitetsregisrering is exited

--- frontend/src/components/kvalitetsregistrering/kvalitetsregistrering.tsx
+++ frontend/src/components/kvalitetsregistrering/kvalitetsregistrering.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { FULLFOER_FIXED_CACHE_KEY, useFullfoerMutation } from '../../redux-api/saksdata';
 import { Footer } from './footer';
 import { Kvalitetsskjema } from './kvalitetsskjema/kvalitetsskjema';
 import { Saksdata } from './saksdata/saksdata';
 
-export const Kvalitetsregistrering = () => (
-  <>
-    <Saksdata />
-    <Kvalitetsskjema />
-    <Footer />
-  </>
-);
+export const Kvalitetsregistrering = () => {
+  const [, { reset }] = useFullfoerMutation({ fixedCacheKey: FULLFOER_FIXED_CACHE_KEY });
+
+  useEffect(() => reset, [reset]);
+
+  return (
+    <>
+      <Saksdata />
+      <Kvalitetsskjema />
+      <Footer />
+    </>
+  );
+};
