4ebe524ce59fcdb6847932776d0a8e206289e6d8
Daniel Eriksson <daniel.eriksson@nav.no>
2021-12-21 09:02:36 +0100

    Move sakstype up, because other fields depends on it

--- frontend/src/components/kvalitetsregistrering/saksdata/saksdata.tsx
+++ frontend/src/components/kvalitetsregistrering/saksdata/saksdata.tsx
@@ -21,8 +21,8 @@ export const Saksdata = () => {
   return (
     <>
       <TilknyttetEnhet />
-      <SakenGjelder />
       <Sakstype />
+      <SakenGjelder />
       <Ytelse />
       <MottattVedtaksinstans />
       <MottattKlageinstans />
