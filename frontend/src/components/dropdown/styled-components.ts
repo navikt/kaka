53159d31d32fcd388791fc8807727836778b14c0
Daniel Eriksson <daniel.eriksson@nav.no>
2021-12-22 08:50:46 +0100

    Make lovhjemmel select wider

--- frontend/src/components/dropdown/styled-components.ts
+++ frontend/src/components/dropdown/styled-components.ts
@@ -43,6 +43,7 @@ export const StyledDropdown = styled.div<StyledDropdownProps>`
   box-shadow: 0 1px 4px 0 rgba(0, 0, 0, 0.3);
   z-index: 1;
   max-height: ${({ maxHeight = '256px' }) => maxHeight};
+  min-width: 275px;
 `;
 
 export const StyledOptionList = styled.ul`
