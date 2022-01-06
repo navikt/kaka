bfd9f031f27fd13c95821e7470a5c703cc49906a
Daniel Eriksson <daniel.eriksson@nav.no>
2022-01-06 07:46:04 +0100

    Simplify import

--- frontend/src/components/app/app.tsx
+++ frontend/src/components/app/app.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import { Header } from '../../components/header/header';
 import { reduxStore } from '../../redux/configure-store';
+import { Header } from '../header/header';
 import { GlobalStyles } from './global-styles';
 import { Router } from './routes';
 import { ScrollReset } from './scroll-reset';
