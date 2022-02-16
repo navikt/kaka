01860c2ddceea12ce475a074613a896d2d9915f6
Christian Skrøvseth <christian.skrovseth@nav.no>
2022-02-16 14:12:13 +0100

    Show saksbehandler only if not own case

--- frontend/src/components/kvalitetsvurdering/saksdata/tilknyttet-enhet.tsx
+++ frontend/src/components/kvalitetsvurdering/saksdata/tilknyttet-enhet.tsx
@@ -1,5 +1,4 @@
 import AlertStripe from 'nav-frontend-alertstriper';
-import { Select } from 'nav-frontend-skjema';
 import React from 'react';
 import styled from 'styled-components';
 import { useCanEdit } from '../../../hooks/use-can-edit';
@@ -7,23 +6,35 @@ import { useEnheter } from '../../../hooks/use-enheter';
 import { useKvalitetsvurderingIsFinished } from '../../../hooks/use-kvalitetsvurdering-is-finished';
 import { useSaksdata } from '../../../hooks/use-saksdata';
 import { useGetUserDataQuery } from '../../../redux-api/metadata';
-import { useSetTilknyttetEnhetMutation } from '../../../redux-api/saksdata';
+import { ISaksdataBase } from '../../../types/saksdata';
 
 export const TilknyttetEnhet = () => {
   const { data: user } = useGetUserDataQuery();
   const [saksdata] = useSaksdata();
-  const [updateTilknyttetEnhet] = useSetTilknyttetEnhetMutation();
+
+  if (typeof saksdata === 'undefined' || typeof user === 'undefined') {
+    return <StyledAlertStripe type="info">Laster...</StyledAlertStripe>;
+  }
+
+  if (saksdata.utfoerendeSaksbehandler === user.ident) {
+    return null;
+  }
+
+  return <ShowUtfoerendeSaksbehandler saksdata={saksdata} />;
+};
+
+interface Props {
+  saksdata: ISaksdataBase;
+}
+
+const ShowUtfoerendeSaksbehandler = ({ saksdata }: Props) => {
   const enheter = useEnheter();
   const canEdit = useCanEdit();
   const isFinished = useKvalitetsvurderingIsFinished();
 
-  if (typeof enheter === 'undefined' || typeof saksdata === 'undefined' || typeof user === 'undefined') {
+  if (typeof enheter === 'undefined') {
     return <StyledAlertStripe type="info">Laster...</StyledAlertStripe>;
   }
-
-  const { klageenheter, navn, ident } = user;
-
-  const name = navn.sammensattNavn ?? ident;
 
   if (isFinished) {
     const enhet = enheter.find((e) => e.navn === saksdata.tilknyttetEnhet)?.beskrivelse ?? 'ingen enhet';
@@ -45,42 +56,7 @@ export const TilknyttetEnhet = () => {
     );
   }
 
-  if (klageenheter.length === 0) {
-    return (
-      <StyledAlertStripe type="advarsel">
-        Du er logget inn som {name}, men har ingen enheter tilgjengelige.
-      </StyledAlertStripe>
-    );
-  }
-
-  if (klageenheter.length === 1) {
-    return (
-      <StyledAlertStripe type="info">
-        Du er logget inn som {name}, tilknyttet {klageenheter[0].beskrivelse}.
-      </StyledAlertStripe>
-    );
-  }
-
-  const onChange = ({ target }: React.ChangeEvent<HTMLSelectElement>) =>
-    updateTilknyttetEnhet({ id: saksdata.id, tilknyttetEnhet: target.value });
-
-  return (
-    <StyledAlertStripe type="info">
-      Du er logget inn som {name}, tilknyttet:
-      <StyledSelect
-        onChange={onChange}
-        bredde="l"
-        data-testid="tilknyttet-enhet-select"
-        value={saksdata.tilknyttetEnhet}
-      >
-        {klageenheter.map((k) => (
-          <option key={k.id} value={k.navn}>
-            {k.beskrivelse}
-          </option>
-        ))}
-      </StyledSelect>
-    </StyledAlertStripe>
-  );
+  return null;
 };
 
 const StyledAlertStripe = styled(AlertStripe)`
@@ -91,9 +67,4 @@ const StyledAlertStripe = styled(AlertStripe)`
   .alertstripe__tekst {
     max-width: none;
   }
-`;
-
-const StyledSelect = styled(Select)`
-  display: inline-block;
-  margin-left: 1em;
 `;
