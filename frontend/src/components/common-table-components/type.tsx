17c1960804dbad28d7fe62fdb5cb68e602c978e6
Daniel Eriksson <eriksson.daniel@protonmail.com>
2022-03-08 07:37:21 +0100

    Set sakstype label colors according to design

--- frontend/src/components/common-table-components/type.tsx
+++ frontend/src/components/common-table-components/type.tsx
@@ -1,18 +1,23 @@
 import { skipToken } from '@reduxjs/toolkit/query/react';
 import React from 'react';
 import { useKodeverkSakstype } from '../../hooks/use-kodeverk-value';
-import { LabelMain } from '../../styled-components/labels';
+import { LabelAnke, LabelKlage } from '../../styled-components/labels';
+import { SakstypeEnum } from '../../types/sakstype';
 
 interface Props {
-  type: string | null;
+  type: SakstypeEnum;
 }
 
 export const Type = ({ type }: Props) => {
   const name = useKodeverkSakstype(type ?? skipToken)?.navn;
 
-  if (typeof name === 'undefined') {
-    return <span>Ikke satt</span>;
+  if (type === SakstypeEnum.KLAGE) {
+    return <LabelKlage>{name}</LabelKlage>;
   }
 
-  return <LabelMain>{name}</LabelMain>;
+  if (type === SakstypeEnum.ANKE) {
+    return <LabelAnke>{name}</LabelAnke>;
+  }
+
+  return <span>Ikke satt</span>;
 };
