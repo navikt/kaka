c1df6fdec6d8553a39870dc1739c7df0418236ab
Christian Skrøvseth <christian.skrovseth@nav.no>
2022-05-05 11:37:47 +0200

    Change filter from 30 days to this month for Total statistics
    Co-authored-by: Daniel Eriksson <eriksson-daniel@users.noreply.github.com>

--- frontend/src/components/filters/date-presets/constants.ts
+++ frontend/src/components/filters/date-presets/constants.ts
@@ -1,6 +1,7 @@
 import dayjs from 'dayjs';
 
 export const NOW = dayjs();
+export const START_OF_MONTH = NOW.startOf('month');
 export const ONE_YEAR_AGO = NOW.subtract(12, 'month');
 export const LAST_YEAR_START = ONE_YEAR_AGO.startOf('year');
 export const LAST_YEAR_END = ONE_YEAR_AGO.endOf('year');
