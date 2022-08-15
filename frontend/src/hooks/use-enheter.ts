14cbb294ef2bebffccfa98e87807d3ffacf1309a
Daniel Eriksson <eriksson.daniel@protonmail.com>
2022-08-15 13:47:19 +0200

    Delete unused files

--- frontend/src/hooks/use-enheter.ts
+++
@@ -1,7 +0,0 @@
-import { useKodeverkValue } from './use-kodeverk-value';
-import { useVikafossenFilter } from './use-vikafossen-filter';
-
-export const useEnheter = () => {
-  const enheter = useKodeverkValue('enheter');
-  return useVikafossenFilter(enheter);
-};
