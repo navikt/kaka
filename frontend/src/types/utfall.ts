63e9c9c03bc6ccb0951d6765f174114975b40954
Christian Skrøvseth <christian.skrovseth@nav.no>
2022-09-30 14:33:15 +0200

    Add innstilling utfall
    Co-authored-by: Daniel Eriksson <eriksson-daniel@users.noreply.github.com>

--- frontend/src/types/utfall.ts
+++ frontend/src/types/utfall.ts
@@ -1,12 +1,14 @@
 export enum UtfallEnum {
-  AVVIST = '8',
-  DELVIS_MEDHOLD = '5',
-  MEDHOLD = '4',
-  OPPHEVET = '3',
-  STADFESTELSE = '6',
-  RETUR = '2',
   TRUKKET = '1',
+  RETUR = '2',
+  OPPHEVET = '3',
+  MEDHOLD = '4',
+  DELVIS_MEDHOLD = '5',
+  STADFESTELSE = '6',
   UGUNST = '7',
+  AVVIST = '8',
+  INNSTILLING_STADFESTELSE = '9',
+  INNSTILLING_AVVIST = '10',
 }
 
 export const isUtfall = (s: string): s is UtfallEnum => Object.values(UtfallEnum).some((e) => e === s);
