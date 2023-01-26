661e97be75c0a929f3a9dd9a0094e49b71789987
Daniel Eriksson <unknown>
2023-01-26 14:08:55 +0100

    Use toLocaleString() to format percentage with one decimal

--- frontend/src/domain/number.ts
+++ frontend/src/domain/number.ts
@@ -1,11 +1,5 @@
-export const toPercent = (n: number, decimals = 1) => {
-  const precision = 10 ** decimals;
-
-  return Math.floor(n * 100 * precision) / precision;
-};
-
-export const round = (n: number, decimals = 1) => {
-  const precision = 10 ** decimals;
-
-  return Math.round(n * precision) / precision;
-};
+export const round = (n: number, decimals = 1) =>
+  n.toLocaleString('nb-no', {
+    minimumFractionDigits: decimals,
+    maximumFractionDigits: decimals,
+  });
