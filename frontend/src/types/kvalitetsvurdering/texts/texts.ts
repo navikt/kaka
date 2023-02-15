c132baea4bd6993aa5c7dcbd7b1675e5aa665a70
Daniel Eriksson <>
2023-02-15 14:24:31 +0100

    Adjust helptext for Annet + add description

--- frontend/src/types/kvalitetsvurdering/texts/texts.ts
+++ frontend/src/types/kvalitetsvurdering/texts/texts.ts
@@ -222,7 +222,9 @@ export const ANNET_TEXTS = {
   annetFritekst: {
     label: 'Annet (valgfri)',
     helpText:
-      'Det du skriver her er kun for klageinstansens interne bruk. Har saken andre avvik som ikke passer med noen av de andre registreringsmulighetene i Kaka, kan du skrive dette her. Husk å skrive kort/med stikkord. Ikke skriv personopplysninger eller detaljer om saken. Du kan også skrive stikkord dersom saken gjelder et typetilfelle.',
+      'Det du skriver her er kun for klageinstansens interne bruk og blir ikke synlig for vedtaksinstansen. Har saken andre avvik som ikke passer med noen av de andre registreringsmulighetene i Kaka, kan du skrive dette her. Husk å skrive kort / med stikkord. Ikke skriv personopplysninger eller detaljer om saken. Du kan også skrive stikkord dersom saken gjelder et typetilfelle.',
+    description:
+      'Det du skriver her er kun synlig for klageinstansen og ikke for vedtaksinstansen. Husk å ikke skrive personopplysninger.',
   },
 };
 
