6ddee8b9aacda4f439462634369c8afff2320b81
Daniel Eriksson <eriksson.daniel@protonmail.com>
2023-05-24 09:34:02 +0200

    Fix hjemler labels in comparison

--- frontend/src/components/statistikk/comparison/hooks/use-labels.ts
+++ frontend/src/components/statistikk/comparison/hooks/use-labels.ts
@@ -1,6 +1,12 @@
 import { useMemo } from 'react';
 import { isoDateToPretty } from '@app/domain/date';
-import { useEnheter, useHjemler, useKlageenheter, useYtelser } from '@app/simple-api-state/use-kodeverk';
+import {
+  RegistreringshjemlerMap,
+  useEnheter,
+  useKlageenheter,
+  useRegistreringshjemlerMap,
+  useYtelser,
+} from '@app/simple-api-state/use-kodeverk';
 import { useSortedUtfall } from '@app/simple-api-state/use-utfall';
 import { IKodeverkSimpleValue } from '@app/types/kodeverk';
 import { AVERAGE, REST } from '../../../filters/comparison/comparison-values/default-options';
@@ -11,7 +17,7 @@ export const useLabels = (): Record<ComparableQueryParams, (value: string) => st
   const { data: enheter = [] } = useEnheter();
   const { data: klageenheter = [] } = useKlageenheter();
   const { data: ytelser = [] } = useYtelser();
-  const { data: hjemler = [] } = useHjemler();
+  const { data: hjemler = {} } = useRegistreringshjemlerMap();
   const { data: utfall = [] } = useSortedUtfall();
 
   return useMemo(
@@ -19,7 +25,7 @@ export const useLabels = (): Record<ComparableQueryParams, (value: string) => st
       [ComparableQueryParams.ENHETER]: (id) => getLabel(id, enheter),
       [ComparableQueryParams.KLAGEENHETER]: (id) => getLabel(id, klageenheter),
       [ComparableQueryParams.YTELSER]: (id) => getLabel(id, ytelser),
-      [ComparableQueryParams.HJEMLER]: (id) => getLabel(id, hjemler),
+      [ComparableQueryParams.HJEMLER]: (id) => getHjemmelLabel(id, hjemler),
       [ComparableQueryParams.VEDTAKSINSTANSGRUPPER]: (id) =>
         getLabel(
           id,
@@ -33,7 +39,7 @@ export const useLabels = (): Record<ComparableQueryParams, (value: string) => st
   );
 };
 
-const getLabel = (value: string, options: IKodeverkSimpleValue<string>[]): string => {
+const getDefaultLabel = (value: string) => {
   if (value === AVERAGE) {
     return 'Gjennomsnitt';
   }
@@ -41,6 +47,10 @@ const getLabel = (value: string, options: IKodeverkSimpleValue<string>[]): strin
   if (value === REST) {
     return 'Resten';
   }
-
-  return options.find((option) => option.id === value)?.navn ?? value;
 };
+
+const getLabel = (value: string, options: IKodeverkSimpleValue<string>[]): string =>
+  getDefaultLabel(value) ?? options.find((option) => option.id === value)?.navn ?? value;
+
+const getHjemmelLabel = (value: string, hjemler: RegistreringshjemlerMap): string =>
+  getDefaultLabel(value) ?? hjemler[value]?.hjemmelnavn ?? value;
