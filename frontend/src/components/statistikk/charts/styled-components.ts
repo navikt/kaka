32bdceef8df1c581e6aea58391f3bb410a18aed9
Daniel Eriksson <eriksson.daniel@protonmail.com>
2023-09-28 09:37:52 +0200

    Fix unknown prop being sent to DOM

--- frontend/src/components/statistikk/charts/styled-components.ts
+++ frontend/src/components/statistikk/charts/styled-components.ts
@@ -1,12 +1,12 @@
 import { styled } from 'styled-components';
 
 interface ChartContainerProps {
-  columns?: 1 | 2 | 3 | 4;
+  $columns?: 1 | 2 | 3 | 4;
 }
 
 export const ChartContainer = styled.div<ChartContainerProps>`
   position: relative;
-  width: ${({ columns = 4 }) => columns * 25}%;
+  width: ${({ $columns = 4 }) => $columns * 25}%;
 `;
 
 export const ChartTitle = styled.h2`
