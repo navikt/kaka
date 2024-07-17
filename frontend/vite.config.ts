33672c725c0f89d6b30a6c556eb3298cf1975dc1
Daniel Eriksson <eriksson.daniel@protonmail.com>
2024-07-17 13:52:48 +0200

    Frontend: Use Bun + Vite

---
+++ frontend/vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [tsconfigPaths(), react()],
+  server: {
+    port: 8062,
+    proxy: {
+      '/api': 'https://kaka.intern.dev.nav.no',
+    },
+  },
+})
+
