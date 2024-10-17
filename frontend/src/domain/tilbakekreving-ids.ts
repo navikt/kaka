a1d36ca5355b7f7982d3a4ec4f1e50dcaf8f32da
Daniel Eriksson <eriksson.daniel@protonmail.com>
2024-10-17 14:49:33 +0200

    Identify more hjemler as tilbakekreving

--- frontend/src/domain/tilbakekreving-ids.ts
+++ frontend/src/domain/tilbakekreving-ids.ts
@@ -1,1 +1,14 @@
-export const TILBAKEKREVING_IDS = ['144', '145', '146', '147', '148', '149', '268'];
+export const TILBAKEKREVING_IDS = [
+  '144',
+  '145',
+  '146',
+  '147',
+  '148',
+  '149',
+  '268',
+  'FORSKL_8',
+  'INNKL_25_T',
+  'INNKL_26A_T',
+  'INNKL_26B_T',
+  'INNKL_29',
+];
