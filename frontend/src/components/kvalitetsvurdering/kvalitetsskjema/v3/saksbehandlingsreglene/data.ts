a582ea8f9e9a7bf10daadd2fa1f79a6f98c6dbd9
Christian Skrøvseth <christian.skrovseth@gmail.com>
2025-10-23 18:00:50 +0200

    Remove empty help texts

--- frontend/src/components/kvalitetsvurdering/kvalitetsskjema/v3/saksbehandlingsreglene/data.ts
+++ frontend/src/components/kvalitetsvurdering/kvalitetsskjema/v3/saksbehandlingsreglene/data.ts
@@ -216,14 +216,12 @@ export const SAKSBEHANDLINGSREGLENE_ERROR_LABELS: Record<SaksbehandlingsregleneE
 };
 export const SAKSBEHANDLINGSREGLENE_HELP_TEXTS: Partial<Record<Fields, string>> = {
   // Veiledningsplikten
-  [VeiledningspliktenBoolean.saksbehandlingsreglerBruddPaaVeiledningsplikten]: '',
   [VeiledningspliktenBoolean.saksbehandlingsreglerVeiledningspliktenPartenHarIkkeFaattSvarPaaHenvendelser]:
     'Du velger denne dersom veiledningen ikke har gitt parten mulighet til å ivareta interessene sine i saken på en god nok måte fordi henvendelser fra parten ikke har blitt besvart, eller har blitt mangelfullt eller misvisende besvart.',
   [VeiledningspliktenBoolean.saksbehandlingsreglerVeiledningspliktenNavHarIkkeGittGodNokVeiledning]:
     'Du velger denne dersom Nav åpenbart skulle gitt veiledning av eget tiltak før det ble fattet vedtak, men dette ikke er gjort, eller Nav har gitt mangelfull eller misvisende veiledning, slik at parten ikke er gitt mulighet til å ivareta interessene sine i saken på en god nok måte.',
 
   // Utredningsplikten
-  [UtredningspliktenBoolean.saksbehandlingsreglerBruddPaaUtredningsplikten]: '',
   [UtredningspliktenBoolean.saksbehandlingsreglerUtredningspliktenUtredningenAvMedisinskeForholdHarIkkeVaertGodNok]:
     'Du velger denne dersom utredningen av saken ikke har gjort trygdemedisinsk faktum tilstrekkelig klart før det ble fattet vedtak. For eksempel dersom det ikke er gjort tilstrekkelige forsøk på å innhente viktige eller oppdaterte helseopplysninger fra behandler. Se også egne spørsmål om kvalitetsavvik i saker med trygdemedisin.',
   [UtredningspliktenBoolean.saksbehandlingsreglerUtredningspliktenUtredningenAvInntektsArbeidsforholdHarIkkeVaertGodNok]:
@@ -238,14 +236,12 @@ export const SAKSBEHANDLINGSREGLENE_HELP_TEXTS: Partial<Record<Fields, string>> 
     'Du velger denne dersom utredningen av saken ikke har gjort faktum om andre forhold enn det som er nevnt ovenfor tilstrekkelig klart før det ble fattet vedtak.',
 
   // Foreleggelsesplikten
-  [ForeleggelsespliktenBoolean.saksbehandlingsreglerBruddPaaForeleggelsesplikten]: '',
   [ForeleggelsespliktenBoolean.saksbehandlingsreglerForeleggelsespliktenUttalelseFraRaadgivendeLegeHarIkkeVaertForelagtParten]:
     'Du velger denne dersom en uttalelse fra rådgivende lege ikke har vært forelagt parten med frist til å uttale seg før det er fattet vedtak som er negativt for parten.',
   [ForeleggelsespliktenBoolean.saksbehandlingsreglerForeleggelsespliktenAndreOpplysningerISakenHarIkkeVaertForelagtParten]:
     'Du velger denne dersom det under saksbehandlingen er mottatt opplysninger som parten har rett til innsyn i og som det ikke er grunnlag for å unnta fra foreleggelse, og disse skulle vært forelagt parten.',
 
   // Begrunnelsesplikten
-  [BegrunnelsespliktenBoolean.saksbehandlingsreglerBruddPaaBegrunnelsesplikten]: '',
   [BegrunnelsespliktenBoolean.saksbehandlingsreglerBegrunnelsespliktenBegrunnelsenViserIkkeTilRegelverket]:
     'Du velger denne dersom vedtaket ikke viser til relevant regelverk eller ikke presenterer innholdet av reglene eller de rettslige problemstillingene på en forståelig nok måte. Jo mer komplisert regelverket eller problemstillingene er, desto mer kreves for å presentere regelinnholdet på en forsvarlig måte.',
   [BegrunnelsespliktenBoolean.saksbehandlingsreglerBegrunnelsespliktenBegrunnelsenNevnerIkkeFaktum]:
@@ -254,7 +250,6 @@ export const SAKSBEHANDLINGSREGLENE_HELP_TEXTS: Partial<Record<Fields, string>> 
     'Du velger denne dersom vedtaket ikke nevner de hensyn som er avgjørende for den lovbundne skjønnsutøvelsen. Enten ved at det ikke vises til relevante deler av rundskriv, eller ved at det ikke er nevnt andre avgjørende hensyn. Dersom vedtaket er negativt for parten, men det er vektige hensyn som taler for og mot partens syn, er det feil å ikke nevne begge deler.',
 
   // Klage og klageforberedelse
-  [KlageOgKlageforberedelsenBoolean.saksbehandlingsreglerBruddPaaRegleneOmKlageOgKlageforberedelse]: '',
   [KlageOgKlageforberedelsenBoolean.saksbehandlingsreglerBruddPaaKlageKlagefristenEllerOppreisningErIkkeVurdertEllerFeilVurdert]:
     'Du velger denne dersom underinstansen har oversett klagefristen eller dersom vurderingen av fristoversittelse eller oppreisning for oversittelsen er feil.',
   [KlageOgKlageforberedelsenBoolean.saksbehandlingsreglerBruddPaaKlageDetErIkkeSoergetForRettingAvFeilIKlagensFormEllerInnhold]:
@@ -263,21 +258,18 @@ export const SAKSBEHANDLINGSREGLENE_HELP_TEXTS: Partial<Record<Fields, string>> 
     'Du velger denne dersom en henvendelse fra parten har blitt behandlet som en klage, til tross for at det er usikkert om det faktisk er ment som en klage. Tilsvarende dersom det er usikkert hva i vedtaket som påklages, og dette burde vært undersøkt. Du velger også denne dersom underinstansen ikke har vurdert nye opplysninger som er tilkommet saken før den oversendes klageinstansen.',
 
   // Omgjøring
-  [OmgjoeringBoolean.saksbehandlingsreglerBruddPaaRegleneOmOmgjoeringUtenforKlageOgAnke]: '',
   [OmgjoeringBoolean.saksbehandlingsreglerOmgjoeringUgyldighetOgOmgjoeringErIkkeVurdertEllerFeilVurdert]:
     'Du velger denne dersom vedtaket er omgjort til skade for parten uten å vurdere om vedtaket er ugyldig og kan omgjøres. Du velger også denne dersom vurderingen av ugyldighet og omgjøring til skade for parten er feil. Tilsvarende dersom det er flere parter i saken, og omgjøringen er til skade for minst en av partene.',
   [OmgjoeringBoolean.saksbehandlingsreglerOmgjoeringDetErFattetVedtakTilTrossForAtBeslutningVarRiktigAvgjoerelsesform]:
     'Du velger denne dersom det er avslått et omgjøringskrav i vedtaks form og gitt klage- eller ankeadgang, til tross for at avgjørelsen egentlig er en prosessledende beslutning som ikke kan påklages/påankes.',
 
   // Journalføringsplikten
-  [JournalfoeringspliktenBoolean.saksbehandlingsreglerBruddPaaJournalfoeringsplikten]: '',
   [JournalfoeringspliktenBoolean.saksbehandlingsreglerJournalfoeringspliktenRelevanteOpplysningerErIkkeJournalfoert]:
     'Du velger denne dersom opplysninger som er relevante for å behandle saken ikke er journalført i Navs godkjente arkivsystem, eller ikke er journalført på den saken som klageinstansen har til behandling. Om dokumentet er et saksdokument som skal journalføres på den aktuelle saken som er til behandling, beror på om dokumentet inneholder opplysninger som er relevante for / inngår i den samme avgjørelsesprosessen.',
   [JournalfoeringspliktenBoolean.saksbehandlingsreglerJournalfoeringspliktenRelevanteOpplysningerHarIkkeGodNokTittelEllerDokumentkvalitet]:
     'Du velger denne dersom tittel på det journalførte dokumentet ikke beskriver innholdet, eller dersom selve dokumentkvaliteten gjør det journalførte dokumentet vanskelig å lese eller kontrollere.',
 
   // Klart språk
-  [KlartSpraakBoolean.saksbehandlingsreglerBruddPaaPliktTilAaKommuniserePaaEtKlartSpraak]: '',
   [KlartSpraakBoolean.saksbehandlingsreglerBruddPaaKlartSprakSpraketIVedtaketErIkkeKlartNok]:
     'Du velger denne dersom innholdet i vedtaket ikke er formidlet på en klar nok måte. For eksempel dersom det er dårlig struktur eller sammenheng i teksten, slik at innholdet blir uklart fordi setningene ikke henger sammen eller vanskelige ord ikke er forklart. Et vedtak som er klart skrevet, blir forstått av mottakeren uten at vedkommende må lese det mange ganger eller er nødt til å bruke hjelpemidler. Klart språk trenger ikke å være fritt for fagord. Det er formålet og målgruppen for vedtaket som avgjør.',
   [KlartSpraakBoolean.saksbehandlingsreglerBruddPaaKlartSprakSpraketIOversendelsesbrevetsErIkkeKlartNok]:
