a4243e6a423741f64594ad8713767c38fca78096
Daniel Eriksson <eriksson.daniel@protonmail.com>
2025-10-30 15:43:24 +0100

    Use native Aksel dropwdown

--- frontend/src/components/filters/comparison/comparison-values/hjemler.tsx
+++ frontend/src/components/filters/comparison/comparison-values/hjemler.tsx
@@ -1,23 +1,15 @@
-import { GroupedSingleSelect } from '@app/components/dropdown/grouped-single-select';
-import type { OptionGroup } from '@app/components/dropdown/types';
 import { AddOptionButton } from '@app/components/filters/comparison/comparison-values/add-option-button';
 import { ColorPicker } from '@app/components/filters/comparison/comparison-values/color-picker';
 import { DEFAULT_OPTIONS } from '@app/components/filters/comparison/comparison-values/default-options';
-import {
-  DropdownContainer,
-  Ellipsis,
-  StyledComparisonItem,
-} from '@app/components/filters/comparison/comparison-values/styled-components';
+import { StyledComparisonItem } from '@app/components/filters/comparison/comparison-values/styled-components';
 import { useOnchange } from '@app/components/filters/comparison/comparison-values/use-onchange';
 import type { ColorToken } from '@app/components/statistikk/colors/token-name';
-import { ToggleButton } from '@app/components/toggle/toggle-button';
-import { useOnClickOutside } from '@app/hooks/use-on-click-outside';
 import { useLovkildeToRegistreringshjemler, useRegistreringshjemlerMap } from '@app/simple-api-state/use-kodeverk';
 import type { IKodeverkSimpleValue } from '@app/types/kodeverk';
 import type { OptionValue } from '@app/types/statistics/common';
-import { TrashIcon } from '@navikt/aksel-icons';
-import { Button } from '@navikt/ds-react';
-import { useMemo, useRef, useState } from 'react';
+import { ChevronDownIcon, TrashIcon } from '@navikt/aksel-icons';
+import { ActionMenu, Button, Heading, VStack } from '@navikt/ds-react';
+import { useMemo } from 'react';
 
 const useNextOption = (data: IKodeverkSimpleValue[]): string | undefined => {
   const { selectedValues } = useOnchange();
@@ -63,15 +55,13 @@ const useLovkildeToRegistreringshjemlerOptions = (selectedValues: OptionValue[])
 
   const selectedIds = useMemo(() => selectedValues.map(([v]) => v), [selectedValues]);
 
-  const hjemmelOptions = useMemo<OptionGroup[]>(
+  const hjemmelOptions = useMemo(
     () =>
       data.map(({ id, navn, registreringshjemler }) => ({
         sectionHeader: { id, name: navn },
-        sectionOptions: registreringshjemler
-          .filter((h) => !selectedIds.includes(h.id))
-          .map((h) => ({ value: h.id, label: h.navn })),
+        sectionOptions: registreringshjemler.map((h) => ({ value: h.id, label: h.navn })),
       })),
-    [data, selectedIds],
+    [data],
   );
 
   const defaultOptions = useMemo(
@@ -91,32 +81,46 @@ const HjemmelSelect = ({ value, color }: { value: string; color: ColorToken }) =
   const { selectedValues, remove, setId, setColor } = useOnchange();
   const options = useLovkildeToRegistreringshjemlerOptions(selectedValues);
 
-  const [isOpen, setIsOpen] = useState(false);
-  const containerRef = useRef<HTMLDivElement>(null);
-
-  useOnClickOutside(() => setIsOpen(false), containerRef);
-
-  const toggleOpen = () => setIsOpen(!isOpen);
-  const closeDropdown = () => setIsOpen(false);
   const { data: hjemler = {} } = useRegistreringshjemlerMap();
 
   const label = hjemler[value]?.hjemmelnavn ?? DEFAULT_OPTIONS.find(({ id }) => id === value)?.navn ?? value;
 
   return (
     <StyledComparisonItem>
-      <DropdownContainer ref={containerRef}>
-        <ToggleButton onClick={toggleOpen} $open={isOpen} $size="small">
-          <Ellipsis>{label}</Ellipsis>
-        </ToggleButton>
-        <GroupedSingleSelect
-          selected={value}
-          onChange={(selected) => setId(value, selected)}
-          options={options}
-          testId="hjemmel-comparison"
-          open={isOpen}
-          close={closeDropdown}
-        />
-      </DropdownContainer>
+      <ActionMenu>
+        <ActionMenu.Trigger>
+          <Button
+            variant="secondary-neutral"
+            icon={<ChevronDownIcon aria-hidden />}
+            iconPosition="right"
+            size="small"
+            className="justify-between! flex grow"
+          >
+            {label}
+          </Button>
+        </ActionMenu.Trigger>
+        <ActionMenu.Content>
+          <VStack gap="2">
+            {options.map(({ sectionHeader, sectionOptions }) => (
+              <VStack key={sectionHeader.id}>
+                <Heading size="small">{sectionHeader.name ?? 'Ukjent'}</Heading>
+                {sectionOptions.map(({ value: hjemmelId, label: hjemmelLabel }) => (
+                  <Button
+                    size="small"
+                    variant={value === hjemmelId ? 'primary-neutral' : 'tertiary-neutral'}
+                    className="justify-start! flex"
+                    key={hjemmelId}
+                    onClick={() => setId(value, hjemmelId)}
+                  >
+                    {hjemmelLabel}
+                  </Button>
+                ))}
+              </VStack>
+            ))}
+          </VStack>
+        </ActionMenu.Content>
+      </ActionMenu>
+
       <ColorPicker color={color} onChange={(newColor) => setColor(value, newColor)} />
       <Button onClick={() => remove(value)} size="small" icon={<TrashIcon aria-hidden />} variant="danger" />
     </StyledComparisonItem>
