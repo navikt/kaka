212eb8a4a390513164e8b77aeb63ecb178f99c14
Daniel Eriksson <11520860+eriksson-daniel@users.noreply.github.com>
2025-12-16 08:51:38 +0100

    Show warning if kvalitetsvurdering stats cannot be shown due to incompatible sakstyper

---
+++ frontend/src/components/statistikk/charts/common/no-kvalitetsvurdering-warning.tsx
@@ -0,0 +1,32 @@
+import { Type } from '@app/components/common-table-components/type';
+import { useSakstypeFilter } from '@app/components/filters/hooks/use-query-filter';
+import { SakstypeEnum } from '@app/types/sakstype';
+import { InformationIcon } from '@navikt/aksel-icons';
+import { BodyShort, Heading, InfoCard, VStack } from '@navikt/ds-react';
+import { CardSize, DynamicCard } from '../../card/card';
+
+export const NoKvalitetsvurderingWarning = () => {
+  const types = useSakstypeFilter();
+
+  return (
+    <DynamicCard size={CardSize.LARGE}>
+      <VStack align="center" justify="center" className="grow">
+        <InfoCard data-color="info">
+          <InfoCard.Header icon={<InformationIcon aria-hidden />}>
+            <Heading size="small">
+              Kvalitetsvurderingstatistikk er kun tilgjengelig for sakstyper <Type type={SakstypeEnum.KLAGE} /> og{' '}
+              <Type type={SakstypeEnum.ANKE} />
+            </Heading>
+          </InfoCard.Header>
+          <InfoCard.Content>
+            <BodyShort>
+              {types.length === 0
+                ? 'For å se statistikken må du velge klage og/eller anke.'
+                : 'For å se statistikken må du filtrere vekk øvrige sakstyper.'}
+            </BodyShort>
+          </InfoCard.Content>
+        </InfoCard>
+      </VStack>
+    </DynamicCard>
+  );
+};
