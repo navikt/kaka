87f897ec3091c102299756755b63132f5385f2c2
Daniel Eriksson <11520860+eriksson-daniel@users.noreply.github.com>
2026-01-05 13:02:29 +0100

    Fix bug where kvalitetsvurdering v3 was not default selected

--- frontend/src/simple-api-state/statistics/v3/use-statistics-my.ts
+++ frontend/src/simple-api-state/statistics/v3/use-statistics-my.ts
@@ -1,10 +1,9 @@
-import type { IStatisticsQuery } from '@app/types/statistics/v2';
-import type { IStatisticsResponseMyV3 } from '@app/types/statistics/v3';
+import type { IStatisticsQuery, IStatisticsResponseMyV3 } from '@app/types/statistics/v3';
 import { skipToken } from '@reduxjs/toolkit/query';
 import { useSimpleApiState } from '../../simple-api-state';
 import { getStateFactory } from '../../state-factory';
 
-const getState = getStateFactory<IStatisticsResponseMyV3, IStatisticsQuery>('/statistics/v2/my');
+const getState = getStateFactory<IStatisticsResponseMyV3, IStatisticsQuery>('/statistics/v3/my');
 
 export const useStatisticsMy = (query: IStatisticsQuery | typeof skipToken) =>
   useSimpleApiState(query === skipToken ? skipToken : getState({ query }));
