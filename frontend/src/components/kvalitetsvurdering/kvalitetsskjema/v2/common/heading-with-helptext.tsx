95f2d7fff5bbd0f3854373487ccd3fc7cb7439c7
Christian Skrøvseth <christian.skrovseth@gmail.com>
2026-04-15 14:18:22 +0200

    Update dependencies

--- frontend/src/components/kvalitetsvurdering/kvalitetsskjema/v2/common/heading-with-helptext.tsx
+++ frontend/src/components/kvalitetsvurdering/kvalitetsskjema/v2/common/heading-with-helptext.tsx
@@ -1,6 +1,5 @@
+import { StyledHeading } from '@app/components/kvalitetsvurdering/kvalitetsskjema/common/styled-components';
 import { type HeadingProps, HelpText, type HelpTextProps } from '@navikt/ds-react';
-import { styled } from 'styled-components';
-import { StyledHeading } from '../../common/styled-components';
 
 interface Props {
   children: string;
@@ -13,15 +12,8 @@ export const HeadingWithHelpText = ({ children, helpText, size = 'small', placem
   helpText === null ? (
     <StyledHeading size="small">{children}</StyledHeading>
   ) : (
-    <Container size={size}>
+    <StyledHeading size={size} className="flex-row">
       {children}
       <HelpText placement={placement}>{helpText}</HelpText>
-    </Container>
+    </StyledHeading>
   );
-
-const Container = styled(StyledHeading)`
-  display: flex;
-  flex-direction: row;
-  gap: 8px;
-  align-items: center;
-`;
