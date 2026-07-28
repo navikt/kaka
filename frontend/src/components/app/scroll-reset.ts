ef8ca21afd5b08b3fa480df9292b676cc814de4b
Christian Skrøvseth <christian.skrovseth@gmail.com>
2026-07-28 12:55:40 +0200

    Fix scroll reset hook

--- frontend/src/components/app/scroll-reset.ts
+++ frontend/src/components/app/scroll-reset.ts
@@ -5,7 +5,9 @@ export const ScrollReset = () => {
   const { pathname } = useLocation();
 
   // biome-ignore lint/correctness/useExhaustiveDependencies: Is supposed to react to pathname changes
-  useEffect(() => window.scrollTo(0, 0), [pathname]);
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
 
   return null;
 };
